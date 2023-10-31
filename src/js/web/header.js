cdb67cf8d4b40c64ddc9cea735e7510d4ad3db7c
nhatme <nhatchimp12@gmail.com>
2023-11-01 00:12:27 +0700

    oke

--- src/js/web/header.js
+++ src/js/web/header.js
@@ -2,86 +2,96 @@ const header = document.querySelector('.header')
 
 header.innerHTML = `
                     <div class="top-header">
-                    <div class="top-header-left">
-                        <div class="fs-11px-fw-600">
-                            <span class="sign-left">24h Vol:</span> <span class="value">1.8M+</span>
-                        </div>
-                        <div class="fs-11px-fw-600">
-                            <span class="sign-left">24h Vol:</span> <span class="value">1.8M+</span>
-                        </div>
-                        <div class="fs-11px-fw-600">
-                            <span class="sign-left">24h Vol:</span> <span class="value">1.8M+</span>
-                        </div>
-                        <div class="fs-11px-fw-600">
-                            <span class="sign-left">24h Vol:</span> <span class="value">1.8M+</span>
-                        </div>
-                        <div class="fs-11px-fw-600">
-                            <span class="sign-left">24h Vol:</span> <span class="value">1.8M+</span>
-                        </div>
-                        <div class="fs-11px-fw-600">
-                            <span class="sign-left">24h Vol:</span> <span class="value">1.8M+</span>
-                        </div>
-                    </div>
-                    <div class="top-header-right">
-                        <div class="top-header-right-half">
-                            <div class="top-header-right-group">
-                                <span class="fs-12px-fw-600">English</span><span class="material-symbols-outlined">
-                                    arrow_drop_down
-                                </span>
+                        <div class="top-header-left">
+                            <div class="fs-11px-fw-600">
+                                <span class="sign-left">Cryptos:</span> <span class="value">0</span>
                             </div>
-                            <div class="item-unit">
+                            <div class="fs-11px-fw-600">
+                                <span class="sign-left">Exchanges:</span> <span class="value">0</span>
+                            </div>
+                            <div class="fs-11px-fw-600">
+                                <span class="sign-left">Market Cap:</span> <span class="value">0</span>
+                            </div>
+                            <div class="fs-11px-fw-600">
+                                <span class="sign-left">Dominance:</span> <span class="value">0</span>
+                            </div>
+                            <div class="fs-11px-fw-600">
+                                <span class="sign-left">ETH Gas:</span> <span class="value">0</span>
+                            </div>
+                            <div class="fs-11px-fw-600">
+                                <span class="sign-left">Fear & Greed:</span> <span class="value">0</span>
+                            </div>
+                        </div>
+                        <div class="top-header-right">
+                            <div class="top-header-right-half">
                                 <div class="top-header-right-group">
-                                    <img src="../src/img/icon.svg" alt="">
+                                    <span class="fs-12px-fw-600">English</span><span class="material-symbols-outlined">
+                                        arrow_drop_down
+                                    </span>
                                 </div>
-                                <span class="fs-12px-fw-600">USD</span>
-                                <span class="material-symbols-outlined">
-                                    arrow_drop_down
-                                </span>
+                                <div class="item-unit">
+                                    <div class="top-header-right-group">
+                                        <img src="../src/img/icon.svg" alt="">
+                                    </div>
+                                    <span class="fs-12px-fw-600">USD</span>
+                                    <span class="material-symbols-outlined">
+                                        arrow_drop_down
+                                    </span>
+                                </div>
                             </div>
+                            <div class="top-header-right-line-straight"></div>
+                            <img src="../src/img/diamond-icon 1.svg" alt="">
+                            <img src="../src/img/notice.svg" alt="">
+                            <img src="../src/img/profile.png" alt="" width="28px">
                         </div>
-                        <div class="top-header-right-line-straight"></div>
-                        <img src="../src/img/diamond-icon 1.svg" alt="">
-                        <img src="../src/img/notice.svg" alt="">
-                        <img src="../src/img/profile.png" alt="" width="28px">
-                    </div>
                     </div>
                     <div class="line-header"></div>
                     <div class="second-header">
-                    <div class="second-header-left">
-                        <div class="second-header-left-logo">
-                            <img src="../src/img/logo/MarketPlaceAsset.svg" alt="">
-                        </div>
-                        <div class="second-header-left-menuBar">
-                            <div>Cryptocurrencies</div>
-                            <div>Exchanges</div>
-                            <div>Community</div>
-                            <div>Products</div>
-                            <div>Learn</div>
-                        </div>
-                    </div>
-                    <div class="second-header-right">
-                        <div class="second-header-right-items">
-                            <div class="second-header-right-item">
-                                <span class="change-color-icon-gg material-symbols-outlined">
-                                    star
-                                </span>
-                                <div class="fs-12px-fw-400">Watchlist</div>
+                        <div class="second-header-left">
+                            <div class="second-header-left-logo">
+                                <img src="../src/img/logo/MarketPlaceAsset.svg" alt="">
                             </div>
-                            <div class="second-header-right-item">
-                                <span class="change-color-icon-gg material-symbols-outlined">
-                                    chart_data
-                                </span>
-                                <div class="fs-12px-fw-400">Portfolio</div>
+                            <div class="second-header-left-menuBar">
+                                <div>Cryptocurrencies</div>
+                                <div>Exchanges</div>
+                                <div>Community</div>
+                                <div>Products</div>
+                                <div>Learn</div>
                             </div>
                         </div>
-                        <div class="second-header-right-searchbar">
-                            <span class="search-icon-gg material-symbols-outlined">
-                                search
-                            </span>
-                            <input class="header-right-searchbar" type="text" placeholder="Search">
-                            <span class="header-right-searchbar-prompt gray-bg">/</span>
+                        <div class="second-header-right">
+                            <div class="second-header-right-items">
+                                <div class="second-header-right-item">
+                                    <span class="change-color-icon-gg material-symbols-outlined">
+                                        star
+                                    </span>
+                                    <div class="fs-12px-fw-400">Watchlist</div>
+                                </div>
+                                <div class="second-header-right-item">
+                                    <span class="change-color-icon-gg material-symbols-outlined">
+                                        chart_data
+                                    </span>
+                                    <div class="fs-12px-fw-400">Portfolio</div>
+                                </div>
+                            </div>
+                            <div class="second-header-right-searchbar">
+                                <span class="search-icon-gg material-symbols-outlined">
+                                    search
+                                </span>
+                                <input class="header-right-searchbar" type="text" placeholder="Search">
+                                <span class="header-right-searchbar-prompt gray-bg">/</span>
+                            </div>
                         </div>
-                    </div>
+                        <div class="second-header-right-rss d_none">
+                            <div class="p_left_16px">
+                                <i class="fa-solid fa-magnifying-glass"></i>
+                            </div>
+                            <img class="p_left_16px" src="../src/img/diamond-icon 1.svg" alt="">
+                            <img class="p_left_16px" src="../src/img/notice.svg" alt="">
+                            <div class="p_left_16px">
+                                <i class="fa-solid fa-bars"></i>
+                            </div>
+                        </div>
                     </div>
                     <div class="line-header"></div>
                     `
