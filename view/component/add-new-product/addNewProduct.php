b441c3b32bf834a2044a1c249b4bcf0b16d9e5b7
nhatme <nhatchimp12@gmail.com>
2023-11-21 15:59:26 +0700

    oke

---
+++ view/component/add-new-product/addNewProduct.php
@@ -0,0 +1,27 @@
+<head>
+    <link rel="stylesheet" href="../css/my-style.css">
+    <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
+</head>
+
+<div class="container">
+    <h3>Add new product</h3>
+    <div>
+
+        <label for="">ID</label>
+        <input type="submit" value="Generate">
+        <label for="">Name</label>
+        <input type="text">
+        <label for="">Symbol</label>
+        <input type="file">
+        <label for="">Image</label>
+        <input type="text">
+        <label for="">Price</label>
+        <input type="text">
+        <label for="">Unit</label>
+        <input type="text">
+        <label for="">Market Cap</label>
+        <input type="text">
+        <label for="">Circulating Supply</label>
+        <input type="text">
+    </div>
+</div>
---
+++ view/component/admin/admin.php
@@ -0,0 +1,191 @@
+<head>
+    <link rel="stylesheet" href="admin.css">
+    <link rel="stylesheet" href="../css/my-style.css">
+    <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
+</head>
+
+<div class="container flex w_100pc">
+
+    <div class="navbar w_15pc p_24px">
+        <div class="navbar-right">
+            <div class="logo mb_48px">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ethereum-icon-purple.svg/1200px-Ethereum-icon-purple.svg.png" alt="" width="32px">
+            </div>
+            <div class="navigation flex f_column">
+                <div class="flex f_column g_32px">
+                    <div class="nav-home flex align_center g_24px">
+                        <iconify-icon class="fs_24px" icon="tabler:home-2"></iconify-icon>
+                        <span class="fs-14px-fw-600">Home</span>
+                    </div>
+                    <div class="nav-assets flex align_center g_24px">
+                        <iconify-icon class="fs_24px" icon="tabler:asset"></iconify-icon>
+                        <span class="fs-14px-fw-600">Assets</span>
+                    </div>
+                    <div class="nav-chart flex align_center g_24px">
+                        <iconify-icon class="fs_24px" icon="uil:chart"></iconify-icon>
+                        <span class="fs-14px-fw-600">Chart</span>
+                    </div>
+                    <div class="nav-home flex align_center g_24px">
+                        <iconify-icon class="fs_24px" icon="tabler:home-2"></iconify-icon>
+                        <span class="fs-14px-fw-600">Home</span>
+                    </div>
+                    <div class="nav-assets flex align_center g_24px">
+                        <iconify-icon class="fs_24px" icon="tabler:asset"></iconify-icon>
+                        <span class="fs-14px-fw-600">Assets</span>
+                    </div>
+                    <div class="nav-chart flex align_center g_24px">
+                        <iconify-icon class="fs_24px" icon="uil:chart"></iconify-icon>
+                        <span class="fs-14px-fw-600">Chart</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="nav-guide flex align_center g_16px">
+            <iconify-icon class="fs_24px" icon="icon-park-outline:guide-board"></iconify-icon>
+            <span class="fs-14px-fw-600">Guide</span>
+        </div>
+    </div>
+    <div class="main w_70pc mt_48px">
+        <div class="path flex align_center g_6px mb_48px h_32px">
+            <span class="fs-14px-fw-500 gray-2nd-color">Dashboard</span> <iconify-icon class="gray-2nd-color" icon="mingcute:right-line"></iconify-icon> <span class="fs-14px-fw-500 gray-2nd-color">Home</span>
+        </div>
+        <div class="mb_48px">
+            <div class="main-title flex_sp_bt mb_32px">
+                <span class="fs-32px-fw-700">ASSETS</span>
+                <div class="flex align_center g_16px">
+                    <span class="fs-16px-fw-400 main-color">More Assets</span>
+                    <iconify-icon class="fs_24px main-color" icon="lucide:move-right"></iconify-icon>
+                </div>
+            </div>
+            <div class="flex_sp_bt" style="display: grid; grid-template-columns: 32% 32% 32%;">
+                <div>
+                    <div class="fs-18px-fw-600 mb_16px main-color">Bitcoin</div>
+                    <div class="main-board-info border_main p_16px radius-8px">
+                        <div>
+                            $1820
+                        </div>
+                        <div>
+                            <span>Profit</span>
+                            <span>Loss</span>
+                            <span>Netral</span>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="fs-18px-fw-600 mb_16px main-color">Bitcoin</div>
+                    <div class="main-board-info border_main p_16px radius-8px">
+                        <div>
+                            $1820
+                        </div>
+                        <div>
+                            <span>Profit</span>
+                            <span>Loss</span>
+                            <span>Netral</span>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="fs-18px-fw-600 mb_16px main-color">Bitcoin</div>
+                    <div class="main-board-info border_main p_16px radius-8px">
+                        <div>
+                            $1820
+                        </div>
+                        <div>
+                            <span>Profit</span>
+                            <span>Loss</span>
+                            <span>Netral</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="main-activity">
+            <div class="main-title flex_sp_bt mb_32px">
+                <span class="fs-32px-fw-700">ACTIVITY</span>
+                <div class="flex align_center g_16px">
+                    <span class="fs-16px-fw-400 main-color">More Assets</span>
+                    <iconify-icon class="fs_24px main-color" icon="lucide:move-right"></iconify-icon>
+                </div>
+            </div>
+
+
+            <div class="border_main radius-8px">
+                <div class="activity-detail  p_16px " style="display: grid; grid-template-columns: 35% 15% 15% 15% 20%;">
+                    <div>
+                        <div class="border-bottom pb_16px fs-14px-fw-700">Transactions</div>
+                        <div class="flex align_center g_8px pt_16px">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ethereum-icon-purple.svg/1200px-Ethereum-icon-purple.svg.png" alt="" width="32px">
+                            <div class="fs-14px-fw-600">Ethereum Purchased</div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="border-bottom pb_16px fs-14px-fw-700">Amount</div>
+                        <div class="pt_16px">0.0154 ETH</div>
+                    </div>
+                    <div>
+                        <div class="border-bottom pb_16px fs-14px-fw-700">Total</div>
+                        <div class="pt_16px">$10.00</div>
+                    </div>
+                    <div>
+                        <div class="border-bottom pb_16px fs-14px-fw-700">Status</div>
+                        <div class="pt_16px">Pending</div>
+                    </div>
+                    <div>
+                        <div class="border-bottom pb_16px fs-14px-fw-700">Date</div>
+                        <div class="pt_16px">February 21, 2023</div>
+                    </div>
+                </div>
+
+                <div class="activity-detail-each" style="display: grid; grid-template-columns: 35% 15% 15% 15% 20%;">
+                    <div>
+                        <div class="flex align_center g_8px pt_16px">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/1200px-Bitcoin.svg.png" alt="" width="32px">
+                            <div class="fs-14px-fw-600">Bitcoin Purchased</div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="pt_16px">0.0154 BTC</div>
+                    </div>
+                    <div>
+                        <div class="pt_16px">$10.00</div>
+                    </div>
+                    <div>
+                        <div class="pt_16px">Pending</div>
+                    </div>
+                    <div>
+                        <div class="pt_16px">February 21, 2023</div>
+                    </div>
+                </div>
+
+                <div class="activity-detail-each" style="display: grid; grid-template-columns: 35% 15% 15% 15% 20%;">
+                    <div>
+                        <div class="flex align_center g_8px pt_16px">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ethereum-icon-purple.svg/1200px-Ethereum-icon-purple.svg.png" alt="" width="32px">
+                            <div class="fs-14px-fw-600">Bitcoin Purchased</div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="pt_16px">0.0154 BTC</div>
+                    </div>
+                    <div>
+                        <div class="pt_16px">$10.00</div>
+                    </div>
+                    <div>
+                        <div class="pt_16px">Pending</div>
+                    </div>
+                    <div>
+                        <div class="pt_16px">February 21, 2023</div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+    </div>
+    <div class="line_straight"></div>
+    <div class="profile w_15pc mt_48px">
+        <p>User Profile</p>
+    </div>
+
+</div>
